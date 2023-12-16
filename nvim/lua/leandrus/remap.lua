399e2143c3db76bca830dc3d81ecb7fea6539d85
Leandrus Fanzec <125384615+leandrusfanzec@users.noreply.github.com>
2023-12-16 12:37:38 -0300

    add compiler/oput lukesmith

--- nvim/lua/leandrus/remap.lua
+++ nvim/lua/leandrus/remap.lua
@@ -2,6 +2,8 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 vim.keymap.set("n", "<leader>o", "<cmd>setlocal spell! spelllang=es<CR>")
+vim.keymap.set("n", "<leader>c", "<cmd>w! | !compiler '%:p'<CR>")
+vim.keymap.set("n", "<leader>p", "<cmd>!opout '%:p'<CR>")
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
